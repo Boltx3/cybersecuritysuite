e65cca15a97ad387dc20c5aee5f155996eb5a0ed
Boltx3 <Dav.h.pham@gmail.com>
2023-12-28 07:07:06 -0600

    Resolved Merge Issues

--- main.go
+++ main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"fmt"
-<<<<<<< Updated upstream
 
 	"github.com/boltx3/cybersecuritysuite/port"
-=======
->>>>>>> Stashed changes
 )
 
 func main() {
 	var hostname string
 	fmt.Println("Port Scanning")
-<<<<<<< Updated upstream
 	fmt.Println("Enter the hostname you want to scan")
 	fmt.Scanln(&hostname)
 	results := port.InitialScan(hostname)
 	results2 := port.ScanPort("TCP", hostname, 80)
 	fmt.Println(results)
 	fmt.Println(results2)
-=======
-	open := scanPort("tcp", "192.168.1.250", 9000)
-	fmt.Printf("Port Open: %t\n", open)
->>>>>>> Stashed changes
 }
